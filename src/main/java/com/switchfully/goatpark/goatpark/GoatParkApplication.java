738e7169a89fc535af04bd382ed14d23a9cbdccf
Jordi Voeten <jordi.voeten@cegeka.com>
2021-12-20 12:10:15 +0100

    Initial commit

---
+++ src/main/java/com/switchfully/goatpark/goatpark/GoatParkApplication.java
@@ -0,0 +1,13 @@
+package com.switchfully.goatpark.goatpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoatParkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoatParkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/switchfully/goatpark/goatpark/GoatParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchfully.goatpark.goatpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoatParkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
